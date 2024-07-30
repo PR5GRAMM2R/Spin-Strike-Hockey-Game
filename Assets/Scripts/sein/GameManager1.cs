09091c921eab17fb1bf3611bbc52ad22bb4a825c
sein12 <moses1008@naver.com>
2024-07-31 06:28:42 +0900

    last commit

--- Assets/Scripts/sein/GameManager1.cs
+++ Assets/Scripts/sein/GameManager1.cs
@@ -11,17 +11,26 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     void Start()
     {
-        if (PhotonNetwork.IsConnectedAndReady)
+        // 방에 접속된 상태에서만 플레이어 객체를 생성
+        if (PhotonNetwork.InRoom)
         {
-            // 플레이어를 랜덤한 스폰 지점에 생성
-            int spawnIndex = Random.Range(0, spawnPoints.Length);
-            PhotonNetwork.Instantiate(playerPrefab.name, spawnPoints[spawnIndex].position, Quaternion.identity);
+            SpawnPlayer();
         }
     }
 
     public override void OnJoinedRoom()
     {
         Debug.Log("Joined Room");
+        SpawnPlayer();
+    }
+
+    void SpawnPlayer()
+    {
+        if (playerPrefab == null)
+        {
+            Debug.LogError("PlayerPrefab is not set in GameManager");
+            return;
+        }
 
         // 플레이어를 랜덤한 스폰 지점에 생성
         int spawnIndex = Random.Range(0, spawnPoints.Length);
